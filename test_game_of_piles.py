4b083f28b72a9bf736f4f04bf39ec484fcfdf698
Chama Abi <anderson.chama.ac@gmail.com>
2023-04-24 02:18:09 +0100

    updated code

--- test_game_of_piles.py
+++ test_game_of_piles.py
@@ -1,31 +1,28 @@
-import gameOfPiles
+import pytest
+from game_of_piles import gameOfPiles
 
-def test_gameOfPiles_sam_wins():
-    # Sam should win with these inputs
-    piles = [3, 5, 7]
+def test_gameOfPiles():
+    # Test case 1
+    piles = [2, 2, 2, 1]
     k = 2
-    assert gameOfPiles.gameOfPiles(piles, k) == "Sam wins the game of 3 pile(s)."
+    assert gameOfPiles(piles, k) == "Alex wins the game of 4 pile(s)."
 
-def test_gameOfPiles_alex_wins():
-    # Alex should win with these inputs
-    piles = [4, 6]
+    # Test case 2
+    piles = [5, 7, 9]
     k = 2
-    assert gameOfPiles.gameOfPiles(piles, k) == "Alex wins the game of 2 pile(s)."
+    assert gameOfPiles(piles, k) == "Alex wins the game of 3 pile(s)."
 
-def test_gameOfPiles_empty_piles():
-    # The function should return an error message if the piles array is empty
-    piles = []
+    # Test case 3
+    piles = [5, 7, 9]
     k = 3
-    assert gameOfPiles.gameOfPiles(piles, k) == "Error: Piles array is empty."
+    assert gameOfPiles(piles, k) == "Sam wins the game of 3 pile(s)."
 
-def test_gameOfPiles_negative_k():
-    # The function should return an error message if k is negative
-    piles = [5, 8, 10]
-    k = -2
-    assert gameOfPiles.gameOfPiles(piles, k) == "Error: k must be a positive integer."
-
-def test_gameOfPiles_zero_stones():
-    # The function should return a message if all piles have zero stones
-    piles = [0, 0, 0]
+    # Test case 4
+    piles = [1, 1, 1]
     k = 2
-    assert gameOfPiles.gameOfPiles(piles, k) == "All piles have zero stones. The game is over."
+    assert gameOfPiles(piles, k) == "Sam wins the game of 3 pile(s)."
+
+    # Test case 5
+    piles = [10, 20, 30, 40]
+    k = 3
+    assert gameOfPiles(piles, k) == "Alex wins the game of 4 pile(s)."
